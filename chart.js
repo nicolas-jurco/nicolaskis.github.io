bbebe7546dafbc6a8f812e238e2b603c0f1fc078
aknicolas <anicolas.kis@gmail.com>
2021-04-29 19:52:21 -0300

    Changed chart responsive Added content on miscellaneous section Added images

--- chart.js
+++ chart.js
@@ -1,25 +1,56 @@
-setTimeout(function start() {
-	console.log("what");
-	let bars = document.getElementsByClassName("bar");
-	console.log(bars.length);
-	for (var i = 0; i < bars.length; i++) {
-		let done = false;
-		let data_percent = document
-			.getElementsByClassName("bar")
-			[i].getAttribute("data-percent");
-		bars[i].style.width = data_percent; // .replace("%", "");
-		bars[i].innerHTML += '<span class="count cf"></span>'; // .replace("%", "");
-		let countElement = bars[i].getElementsByClassName("count");
-		let countMax = parseInt(data_percent.replace("%", ""));
-		let j = 0;
-		let timer = setInterval(function () {
-			if (countMax == j) {
-				clearInterval(timer);
-				j = 0;
-			} else {
-				countElement[0].innerHTML =
-					'<span class="count cf">' + j++ + "%</span>"; // .replace("%", "");
-			}
-		}, 20);
-	}
-}, 50);
+// (function () {
+// 	"use strict";
+
+// 	// define variables
+// 	var items = document.querySelectorAll(".timeline li");
+
+// 	function isElementInViewport(el) {
+// 		var rect = el.getBoundingClientRect();
+// 		return (
+// 			rect.top >= 0 &&
+// 			rect.left >= 0 &&
+// 			rect.bottom <=
+// 				(window.innerHeight || document.documentElement.clientHeight) &&
+// 			rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+// 		);
+// 	}
+
+// 	function callbackFunc() {
+// 		for (var i = 0; i < items.length; i++) {
+// 			if (isElementInViewport(items[i])) {
+// 				items[i].classList.add("in-view");
+// 			}
+// 		}
+// 	}
+
+// 	// listen for events
+// 	window.addEventListener("load", callbackFunc);
+// 	window.addEventListener("resize", callbackFunc);
+// 	window.addEventListener("scroll", callbackFunc);
+// })();
+
+// setTimeout(function start() {
+// 	console.log("what");
+// 	let bars = document.getElementsByClassName("bar");
+// 	console.log(bars.length);
+// 	for (var i = 0; i < bars.length; i++) {
+// 		let done = false;
+// 		let data_percent = document
+// 			.getElementsByClassName("bar")
+// 			[i].getAttribute("data-percent");
+// 		bars[i].style.width = data_percent; // .replace("%", "");
+// 		bars[i].innerHTML += '<span class="count cf"></span>'; // .replace("%", "");
+// 		let countElement = bars[i].getElementsByClassName("count");
+// 		let countMax = parseInt(data_percent.replace("%", ""));
+// 		let j = 0;
+// 		let timer = setInterval(function () {
+// 			if (countMax == j) {
+// 				clearInterval(timer);
+// 				j = 0;
+// 			} else {
+// 				countElement[0].innerHTML =
+// 					'<span class="count cf">' + j++ + "%</span>"; // .replace("%", "");
+// 			}
+// 		}, 20);
+// 	}
+// }, 50);
--- timeline.js
+++ timeline.js
@@ -1,8 +1,7 @@
 (function () {
-	"use strict";
+	("use strict");
 
 	// define variables
-	var items = document.querySelectorAll(".timeline li");
 
 	function isElementInViewport(el) {
 		var rect = el.getBoundingClientRect();
@@ -16,9 +15,39 @@
 	}
 
 	function callbackFunc() {
+		var items = document.querySelectorAll(".timeline li");
 		for (var i = 0; i < items.length; i++) {
 			if (isElementInViewport(items[i])) {
 				items[i].classList.add("in-view");
+			}
+		}
+		let bars = document.getElementsByClassName("bar");
+		for (var i = 0; i < bars.length; i++) {
+			if (
+				isElementInViewport(bars[i]) &&
+				!bars[i].classList.contains("completed")
+			) {
+				// for (var i = 0; i < bars.length; i++) {
+				let done = false;
+				let data_percent = document
+					.getElementsByClassName("bar")
+					[i].getAttribute("data-percent");
+				bars[i].style.width = data_percent; // .replace("%", "");
+				bars[i].innerHTML += '<span class="count cf"></span>'; // .replace("%", "");
+				let countElement = bars[i].getElementsByClassName("count");
+				let countMax = parseInt(data_percent.replace("%", ""));
+				let j = 0;
+				let timer = setInterval(function () {
+					if (countMax == j) {
+						clearInterval(timer);
+						j = 0;
+					} else {
+						countElement[0].innerHTML =
+							'<span class="count cf">' + j++ + "%</span>"; // .replace("%", "");
+					}
+				}, 20);
+				bars[i].classList.add("completed");
+				// }
 			}
 		}
 	}
